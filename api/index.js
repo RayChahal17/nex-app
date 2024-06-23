20f7385ff85c86b10849b5e6d5b4d2a70df4077e
RayChahal17 <152761723+RayChahal17@users.noreply.github.com>
2024-06-23 18:07:51 -0400

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import estimateRoutes from './routes/estimate.route.js';
-import contactRoutes from './routes/contact.route.js';  // <-- Include this line
-import { errorHandler } from './utils/error.js';
+import contactRoutes from './routes/contact.route.js';
 import busyTimeRoutes from './routes/busyTime.route.js';
-// import busyTimeUserRoutes from './routes/busyTimeUser.route.js';  // Comment out if not needed
-// import confirmedAppointmentsRoutes from './routes/confirmedAppointmenets.route.js';  // Comment out if not needed
-import appointmentRoutes from './routes/appointment.routes.js';  // Ensure this is included
-import path from 'path';
+import calcEstimateRoutes from './routes/calcEstimate.route.js';
+import appointmentRoutes from './routes/appointment.routes.js';
+import { errorHandler } from './utils/error.js';
+
 dotenv.config();
 
 mongoose
@@ -27,22 +27,29 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.json());
+// Increase payload size limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Define API routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/estimate', estimateRoutes);
-app.use('/api/contact', contactRoutes);  // <-- Include this line
+app.use('/api/contact', contactRoutes);
 app.use('/api/busy-times', busyTimeRoutes);
-// app.use('/api/busy-time-user', busyTimeUserRoutes);  // Comment out if not needed
-// app.use('/api/confirmed-appointments', confirmedAppointmentsRoutes);  // Comment out if not needed
-app.use('/api', appointmentRoutes);  // Ensure this is included
+app.use('/api/calc-estimate', calcEstimateRoutes);
+app.use('/api', appointmentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
- });
+});
 
+
+
+// Error handling middleware
 app.use((err, req, res, next) => {
    const statusCode = err.statusCode || 500;
    const message = err.message || 'Internal Server Error';
@@ -54,6 +61,7 @@ app.use((err, req, res, next) => {
    });
 });
 
-app.listen(3000, () => {
-   console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
 });
